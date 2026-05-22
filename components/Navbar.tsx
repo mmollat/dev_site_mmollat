8d113483b143d0571dbb9272986f1e8a3784edaf
mmollat <77718599+mmollat@users.noreply.github.com>
2026-05-21 22:48:30 -0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,1 +1,35 @@
-tee
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <header className="nav">
+      <a href="/">
+        <Image
+          src="/images/ibarra-logo.png"
+          alt="Ibarra Custom Rods"
+          width={250}
+          height={90}
+          priority
+        />
+      </a>
+
+      <nav>
+        <a href="/">Home</a>
+
+        <a href="/#rods">Rods</a>
+
+        <a href="/#process">Build Process</a>
+
+        <a href="/#gallery">Gallery</a>
+
+        <a href="/about">About</a>
+
+        <a href="/#contact">Contact</a>
+      </nav>
+
+      <a href="/#contact" className="outlineBtn">
+        Build Your Rod
+      </a>
+    </header>
+  );
+}
